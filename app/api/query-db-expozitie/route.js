c61b6b2374b060f41b4d8fad5173c1b87d658594
Alex <alexsala112@gmail.com>
2024-03-11 14:11:31 +0200

    refresh3

--- app/api/query-db-expozitie/route.js
+++ app/api/query-db-expozitie/route.js
@@ -5,12 +5,12 @@ import { NextResponse } from "next/server"
 export async function GET() {
   try {
     const result = await client.sql`SELECT * FROM paintings;`
-    return new NextResponse.json(result.rows).withHeader(
+    return NextResponse.json(result.rows).withHeader(
       "Cache-Control",
       "s-maxage=0, stale-while-revalidate"
     )
   } catch (error) {
-    return new NextResponse.json(
+    return NextResponse.json(
       { error: error.message },
       { status: 500 }
     ).withHeader("Cache-Control", "s-maxage=0, stale-while-revalidate")
