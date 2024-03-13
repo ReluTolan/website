f735e4e5df73451562c73816e59adb6d6ea92027
Alex <alexsala112@gmail.com>
2024-03-13 17:01:33 +0200

    v2

--- app/api/query-db-expozitie/route.js
+++ app/api/query-db-expozitie/route.js
@@ -1,12 +1,14 @@
 import { db } from "@vercel/postgres"
-const client = await db.connect()
 import { NextResponse } from "next/server"
 
 export async function GET() {
+  const client = await db.connect()
   try {
     const result = await client.sql`SELECT * FROM paintings;`
     return NextResponse.json(result.rows)
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 })
+  } finally {
+    await client.end()
   }
 }
